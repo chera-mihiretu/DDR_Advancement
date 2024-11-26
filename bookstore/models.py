5ecb9d29866c48a6a707f970d55bc13ae424deae
chera-mihiretu <cheramihiretucm@gmail.com>
2024-11-26 05:58:21 -0500

    prepare pages for the

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from typing import *
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    publication_date = models.DatinseField()
+    pages = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    genre = models.CharField(max_length=200)
+    def __str__(self) -> str:
+        return self.title
+
+
+class BookComments(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    comment = models.TextField()
+    def __str__(self) -> str:
+        return self.comment
+
+class BookLikes(models.Model):
+    book = models.FeriegnKey(Book, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+    def __str__(self) -> str:
+        return str(self.likes)
--- bookstore/views.py
+++ bookstore/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def addNewBook(request):
+    pass
+
+def updateExistingBook(request):
+    pass
+
+def deleteBook(request):
+    pass
+
+def likeBook(request):
+    pass
+
+def commentOnBook(request):
+    pass
